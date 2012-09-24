6cc3f2915a51fdd261dd3eb896a64afaca72dd08
ron975 <ronny6993@gmail.com>
2012-09-24 10:51:24 -0400

    Moved CommandExecutor to separate class and cleaned it up, added some Javadocs for fun :)

--- src/main/java/net/mystia/PumpkiNibble/PumpkiNibbleConfig.java
+++ src/main/java/net/mystia/PumpkiNibble/PumpkiNibbleConfig.java
@@ -4,15 +4,26 @@ import java.util.HashMap;
 import java.util.List;
 
 import net.mystia.PumpkiNibble.PumpkiNibbleMain;
-
+/** PumpkiNibble
+ * PumpkiNibbleConfig gets and sets the configuration options for the plugin
+ * As close of an API as you're ever gonna get.
+ * @author ron975
+ * 
+ */
 public class PumpkiNibbleConfig {
 
 	public HashMap<String, List<String>> settings;
 	private PumpkiNibbleMain plugin;
 
+	/**
+	 * Gets potion durations from config.yml
+	 * @param type String
+	 * @param effect String
+	 * @return potionDuration As Integer
+	 */
 	public Integer getPotionDuration(String type, String effect) {
 		Integer potionDuration = plugin.getConfig().getInt(
-				type + ".potionDuration." + effect);
+				"items."+ type + ".potionDuration." + effect);
 		if (potionDuration == null) {
 			potionDuration = 0;
 		}
@@ -20,78 +31,140 @@ public class PumpkiNibbleConfig {
 		return potionDuration;
 	}
 
+	/**
+	 * Gets potion strength from config.yml
+	 * @param type String
+	 * @param effect String
+	 * @return potionStrength As Integer
+	 */
 	public Integer getPotionStrength(String type, String effect) {
 		Integer potionStrength = plugin.getConfig().getInt(
-				type + ".potionStrength." + effect);
+				"items."+type + ".potionStrength." + effect);
 		if (potionStrength == null) {
 			potionStrength = 0;
 		}
 		return potionStrength;
 	}
 
+	/**
+	 * Gets potion effects as List from config.yml
+	 * @param type String
+	 * @return potionEffects As List<String>
+	 */
 	public List<String> getPotionEffects(String type) {
 		List<String> potionEffects = plugin.getConfig().getStringList(
-				type + ".potionEffects");
+				"items."+type + ".potionEffects");
 		return potionEffects;
 	}
 
+	/**
+	 * Gets message shown when an item is eaten from config.yml
+	 * @param type String
+	 * @return eatMessage As String
+	 */
 	public String getEatMessage(String type) {
 		String eatMessage = plugin.getConfig()
-				.getString(type + ".messageOnEat");
+				.getString("items."+ type + ".messageOnEat");
 		return eatMessage;
 	}
 
+	/**
+	 * Gets message shown when there is not enough items to be eaten
+	 * @param type String
+	 * @return insufficientMessage As String
+	 */
 	public String getInsufficientMessage(String type) {
 		String insufficientMessage = plugin.getConfig().getString(
-				type + ".messageInsufficient");
+				"items."+type + ".messageInsufficient");
 		return insufficientMessage;
 	}
 
+	/**
+	 * Gets amount of food to be healed
+	 * @param type String
+	 * @return healFood As Integer
+	 */
 	public Integer getHealFoodAmount(String type) {
-		Integer healFood = plugin.getConfig().getInt(type + ".healFoodAmount");
+		Integer healFood = plugin.getConfig().getInt("items."+type + ".healFoodAmount");
 		if (healFood == null) {
 			healFood = 0;
 		}
 		return healFood;
 	}
 
+	/**
+	 * Gets amount of health to be healed
+	 * @param type String
+	 * @return healHeath As Integer
+	 */
 	public Integer getHealHealthAmount(String type) {
 		Integer healHealth = plugin.getConfig().getInt(
-				type + ".healHealthAmount");
+				"items."+type + ".healHealthAmount");
 		if (healHealth == null) {
 			healHealth = 0;
 		}
 		return healHealth;
 	}
 
+	/**
+	 * Gets amount of item to be eaten
+	 * @param type String
+	 * @return itemAmount AS Integer
+	 */
 	public Integer getItemAmount(String type) {
-		Integer itemAmount = plugin.getConfig().getInt(type + ".itemAmount");
+		Integer itemAmount = plugin.getConfig().getInt("items."+type + ".itemAmount");
 		if (itemAmount == null) {
 			itemAmount = 1;
 		}
 		return itemAmount;
 	}
-
+	/**
+	 * Builds permission string cleanly for use
+	 * @param permissionSection
+	 * @param type String
+	 * @return permission As String
+	 */
 	public String getPermission(String permissionSection, String type) {
 		String permission = "pumpkinibble." + permissionSection + "." + type;
 		permission = permission.toLowerCase();
 		return permission;
 	}
 
+	/**
+	 * Checks if a certain item is enabled in config
+	 * @param type String
+	 * @return ifEnabled As boolean
+	 */
 	public boolean isEnabled(String type) {
-		boolean ifEnabled = plugin.getConfig().getBoolean(type + ".enabled");
+		boolean ifEnabled = plugin.getConfig().getBoolean("items."+type + ".enabled");
 		return ifEnabled;
 	}
+	/**
+	 * Gets message shown when someone is unable to eat something
+	 * @param type String
+	 * @return unableMessage As String
+	 */
 	public String getUnableMessage(String type){
-		String unableMessage = plugin.getConfig().getString(type+".messageUnable");
+		String unableMessage = plugin.getConfig().getString("items."+type+".messageUnable");
 		return unableMessage;
 	}
+	/**
+	 * Gets personal toggle settings from HashMap
+	 * @param playername
+	 * @return personalSettings As List<String>
+	 */
 	public List<String> getPersonalSettings(String playername){
 		List<String> personalSettings = settings.get(playername);
 		return personalSettings;
 		
 	}
 	
+	/**
+	 * Sets personal settings to HashMap and returns whether item is enabled or disabled
+	 * @param playername
+	 * @param setting
+	 * @return boolean
+	 */
 	public boolean setPersonalSettings(String playername, String setting){
 		List<String> personalSettings = getPersonalSettings(playername);
 		if (!personalSettings.contains(setting)){
@@ -104,5 +177,30 @@ public class PumpkiNibbleConfig {
 		return false;
 		}
 		
+	}
+	/**
+	 * Gets valid items from config as List<String>
+	 * @return validItems As List<String>
+	 */
+	public List<String> getValidItems(){
+		List<String> validItems = plugin.getConfig().getStringList("items");
+		return validItems;
+	}
+
+	/**
+	 * Parses List of valid items and returns string of items separated by separator parameter
+	 * @param separator String
+	 * @return validItems As String
+	 */
+	public String getValidItemsAsString(String separator){
+		String validItems = null;
+		for (String item : this.getValidItems()){
+			if (validItems == null){
+				validItems = item;
+			}else{
+				validItems = validItems + separator + item;
+			}
+		}
+		return validItems;
 	}
 }
--- src/main/java/net/mystia/PumpkiNibble/PumpkiNibbleListener.java
+++ src/main/java/net/mystia/PumpkiNibble/PumpkiNibbleListener.java
@@ -29,6 +29,7 @@ public class PumpkiNibbleListener implements Listener {
 						&& event.getClickedBlock().getType() != Material.STONE_BUTTON
 						&& event.getClickedBlock().getType() != Material.LEVER && event
 						.getClickedBlock().getType() != Material.TRAP_DOOR)) {
+			/* Check for items */
 			if (player.getItemInHand() != null) {
 				if (player.getItemInHand().getType() == Material.PUMPKIN_SEEDS) {
 					type = "pumpkin";
@@ -51,211 +52,282 @@ public class PumpkiNibbleListener implements Listener {
 									} else {
 										if (player.getItemInHand().getType() == Material.FERMENTED_SPIDER_EYE) {
 											type = "fermentedeye";
-										}else{
-											if (player.getItemInHand().getType() == Material.SLIME_BALL){
+										} else {
+											if (player.getItemInHand()
+													.getType() == Material.SLIME_BALL) {
 												type = "slimeball";
+											} else {
+												if (player.getItemInHand()
+														.getType() == Material.SUGAR) {
+													type = "sugar";
+												} else {
+													if (player.getItemInHand()
+															.getType() == Material.SPECKLED_MELON) {
+														type = "goldmelon";
+													} else {
+														/*
+														 * Sugarcane and
+														 * Mushrooms can be
+														 * planted on Grass and
+														 * Dirt, and Mycelium,
+														 * so we check for those
+														 */
+														if (event
+																.getClickedBlock()
+																.getType() != Material.DIRT
+																&& event.getClickedBlock()
+																		.getType() != Material.GRASS
+																&& event.getClickedBlock()
+																		.getType() == Material.MYCEL) {
+															if (event
+																	.getClickedBlock()
+																	.getType() == Material.SUGAR_CANE) {
+																type = "sugarcane";
+
+															} else {
+																if (event
+																		.getClickedBlock()
+																		.getType() == Material.BROWN_MUSHROOM) {
+																	type = "brownmushroom";
+																} else {
+																	if (event
+																			.getClickedBlock()
+																			.getType() == Material.RED_MUSHROOM) {
+																		type = "redmushroom";
+																	}
+																}
+															}
+
+														}
+													}
+												}
 											}
 										}
 									}
 								}
-							}
 
+							}
+						}
+
+						/* Check if the type is a valid item or null */
+						if (type == null
+								|| !config.getValidItems().contains(type)) {
+							return;
 						}
 					}
+					String messageUnable = config.getUnableMessage(type);
+					String messageOnEat = config.getEatMessage(type);
+					String messageInsufficient = config
+							.getInsufficientMessage(type);
+					List<String> potionEffects = config.getPotionEffects(type);
+					boolean isEnabled = config.isEnabled(type);
+					int healFoodAmount = config.getHealFoodAmount(type);
+					int healHealthAmount = config.getHealHealthAmount(type);
+					int seedAmount = config.getItemAmount(type);
+					String permission = config.getPermission("nibble", type);
 
-					if (type == null) {
+					if (!isEnabled) {
 						return;
-					}
-				}
-				String messageUnable = config.getUnableMessage(type);
-				String messageOnEat = config.getEatMessage(type);
-				String messageInsufficient = config
-						.getInsufficientMessage(type);
-				List<String> potionEffects = config.getPotionEffects(type);
-				boolean isEnabled = config.isEnabled(type);
-				int healFoodAmount = config.getHealFoodAmount(type);
-				int healHealthAmount = config.getHealHealthAmount(type);
-				int seedAmount = config.getItemAmount(type);
-				String permission = config.getPermission("nibble", type);
-
-				if (!isEnabled) {
-					return;
-				} else {
-
-					if (player.hasPermission(permission)) {
-						if (!config.getPersonalSettings(event.getPlayer().getName()).contains(type)){
-						if (player.getItemInHand().getAmount() >= seedAmount) {
-							int ItemAmount = player.getItemInHand().getAmount();
-							if (ItemAmount == seedAmount) {
-								player.getInventory().remove(
-										player.getItemInHand());
-							} else {
-								player.getItemInHand().setAmount(
-										ItemAmount - seedAmount);
-							}
-							player.sendMessage(ChatColor.GOLD + messageOnEat);
-
-							int FoodLevel = player.getFoodLevel();
-							int HealthLevel = player.getHealth();
-							int NewFoodLevel = FoodLevel + healFoodAmount;
-							int NewHealthLevel = HealthLevel + healHealthAmount;
-							player.setFoodLevel(NewFoodLevel);
-							player.setHealth(NewHealthLevel);
-
-							if (potionEffects.contains("regen")) {
-
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.REGENERATION, config
-												.getPotionDuration(type,
-														"regen"), config
-												.getPotionStrength(type,
-														"regen")));
-							}
-							if (potionEffects.contains("heal")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.HEAL,
-										config.getPotionDuration(type, "heal"),
-										config.getPotionStrength(type, "heal")));
-							}
-
-							if (potionEffects.contains("harm")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.HARM,
-										config.getPotionDuration(type, "harm"),
-										config.getPotionStrength(type, "harm")));
-							}
-
-							if (potionEffects.contains("poison")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.POISON, config
-												.getPotionDuration(type,
-														"poison"), config
-												.getPotionStrength(type,
-														"poison")));
-							}
-							if (potionEffects.contains("hunger")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.HUNGER, config
-												.getPotionDuration(type,
-														"hunger"), config
-												.getPotionStrength(type,
-														"hunger")));
-							}
-							if (potionEffects.contains("nightvision")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.NIGHT_VISION, config
-												.getPotionDuration(type,
-														"nightvision"), config
-												.getPotionStrength(type,
-														"nightvision")));
-							}
-
-							if (potionEffects.contains("weakness")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.WEAKNESS, config
-												.getPotionDuration(type,
-														"weakness"), config
-												.getPotionStrength(type,
-														"weakness")));
-							}
-							if (potionEffects.contains("jump")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.JUMP,
-										config.getPotionDuration(type, "jump"),
-										config.getPotionStrength(type, "jump")));
-							}
-							if (potionEffects.contains("resistance")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.DAMAGE_RESISTANCE,
-										config.getPotionDuration(type,
-												"resistance"), config
-												.getPotionStrength(type,
-														"resistance")));
-							}
-							if (potionEffects.contains("fireresist")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.FIRE_RESISTANCE,
-										config.getPotionDuration(type,
-												"fireresist"), config
-												.getPotionStrength(type,
-														"fireresist")));
-							}
-							if (potionEffects.contains("invisibility")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.INVISIBILITY, config
-												.getPotionDuration(type,
-														"invisibility"), config
-												.getPotionStrength(type,
-														"invisibility")));
-							}
-							if (potionEffects.contains("slowmine")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.SLOW_DIGGING, config
-												.getPotionDuration(type,
-														"slowmine"), config
-												.getPotionStrength(type,
-														"slowmine")));
-							}
-							if (potionEffects.contains("slow")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.SLOW,
-										config.getPotionDuration(type, "slow"),
-										config.getPotionStrength(type, "slow")));
-							}
-							if (potionEffects.contains("fastmine")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.FAST_DIGGING, config
-												.getPotionDuration(type,
-														"fastmine"), config
-												.getPotionStrength(type,
-														"fastmine")));
-							}
-							if (potionEffects.contains("waterbreathing")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.WATER_BREATHING,
-										config.getPotionDuration(type,
-												"waterbreathing"), config
-												.getPotionStrength(type,
-														"waterbreathing")));
-							}
-							if (potionEffects.contains("speed")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.SPEED, config
-												.getPotionDuration(type,
-														"speed"), config
-												.getPotionStrength(type,
-														"speed")));
-							}
-							if (potionEffects.contains("nausea")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.CONFUSION, config
-												.getPotionDuration(type,
-														"nausea"), config
-												.getPotionStrength(type,
-														"nausea")));
-							}
-							if (potionEffects.contains("blindness")) {
-								player.addPotionEffect(new PotionEffect(
-										PotionEffectType.BLINDNESS, config
-												.getPotionDuration(type,
-														"blindness"), config
-												.getPotionStrength(type,
-														"blindness")));
-							}
-						} else {
-
-							player.sendMessage(ChatColor.GOLD
-									+ messageInsufficient);
-							player.getInventory()
-									.remove(player.getItemInHand());
-
-						}
 					} else {
-						player.sendMessage(ChatColor.GOLD + messageUnable);
+
+						if (player.hasPermission(permission)) {
+							if (!config.getPersonalSettings(
+									event.getPlayer().getName()).contains(type)) {
+								if (player.getItemInHand().getAmount() >= seedAmount) {
+									int ItemAmount = player.getItemInHand()
+											.getAmount();
+									if (ItemAmount == seedAmount) {
+										player.getInventory().remove(
+												player.getItemInHand());
+									} else {
+										player.getItemInHand().setAmount(
+												ItemAmount - seedAmount);
+									}
+									player.sendMessage(ChatColor.GOLD
+											+ messageOnEat);
+
+									int FoodLevel = player.getFoodLevel();
+									int HealthLevel = player.getHealth();
+									int NewFoodLevel = FoodLevel
+											+ healFoodAmount;
+									int NewHealthLevel = HealthLevel
+											+ healHealthAmount;
+									player.setFoodLevel(NewFoodLevel);
+									player.setHealth(NewHealthLevel);
+
+									if (potionEffects.contains("regen")) {
+
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.REGENERATION,
+												config.getPotionDuration(type,
+														"regen"), config
+														.getPotionStrength(
+																type, "regen")));
+									}
+									if (potionEffects.contains("heal")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.HEAL, config
+														.getPotionDuration(
+																type, "heal"),
+												config.getPotionStrength(type,
+														"heal")));
+									}
+
+									if (potionEffects.contains("harm")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.HARM, config
+														.getPotionDuration(
+																type, "harm"),
+												config.getPotionStrength(type,
+														"harm")));
+									}
+
+									if (potionEffects.contains("poison")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.POISON,
+												config.getPotionDuration(type,
+														"poison"), config
+														.getPotionStrength(
+																type, "poison")));
+									}
+									if (potionEffects.contains("hunger")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.HUNGER,
+												config.getPotionDuration(type,
+														"hunger"), config
+														.getPotionStrength(
+																type, "hunger")));
+									}
+									if (potionEffects.contains("nightvision")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.NIGHT_VISION,
+												config.getPotionDuration(type,
+														"nightvision"), config
+														.getPotionStrength(
+																type,
+																"nightvision")));
+									}
+
+									if (potionEffects.contains("weakness")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.WEAKNESS,
+												config.getPotionDuration(type,
+														"weakness"), config
+														.getPotionStrength(
+																type,
+																"weakness")));
+									}
+									if (potionEffects.contains("jump")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.JUMP, config
+														.getPotionDuration(
+																type, "jump"),
+												config.getPotionStrength(type,
+														"jump")));
+									}
+									if (potionEffects.contains("resistance")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.DAMAGE_RESISTANCE,
+												config.getPotionDuration(type,
+														"resistance"), config
+														.getPotionStrength(
+																type,
+																"resistance")));
+									}
+									if (potionEffects.contains("fireresist")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.FIRE_RESISTANCE,
+												config.getPotionDuration(type,
+														"fireresist"), config
+														.getPotionStrength(
+																type,
+																"fireresist")));
+									}
+									if (potionEffects.contains("invisibility")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.INVISIBILITY,
+												config.getPotionDuration(type,
+														"invisibility"), config
+														.getPotionStrength(
+																type,
+																"invisibility")));
+									}
+									if (potionEffects.contains("slowmine")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.SLOW_DIGGING,
+												config.getPotionDuration(type,
+														"slowmine"), config
+														.getPotionStrength(
+																type,
+																"slowmine")));
+									}
+									if (potionEffects.contains("slow")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.SLOW, config
+														.getPotionDuration(
+																type, "slow"),
+												config.getPotionStrength(type,
+														"slow")));
+									}
+									if (potionEffects.contains("fastmine")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.FAST_DIGGING,
+												config.getPotionDuration(type,
+														"fastmine"), config
+														.getPotionStrength(
+																type,
+																"fastmine")));
+									}
+									if (potionEffects
+											.contains("waterbreathing")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.WATER_BREATHING,
+												config.getPotionDuration(type,
+														"waterbreathing"),
+												config.getPotionStrength(type,
+														"waterbreathing")));
+									}
+									if (potionEffects.contains("speed")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.SPEED, config
+														.getPotionDuration(
+																type, "speed"),
+												config.getPotionStrength(type,
+														"speed")));
+									}
+									if (potionEffects.contains("nausea")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.CONFUSION,
+												config.getPotionDuration(type,
+														"nausea"), config
+														.getPotionStrength(
+																type, "nausea")));
+									}
+									if (potionEffects.contains("blindness")) {
+										player.addPotionEffect(new PotionEffect(
+												PotionEffectType.BLINDNESS,
+												config.getPotionDuration(type,
+														"blindness"), config
+														.getPotionStrength(
+																type,
+																"blindness")));
+									}
+								} else {
+
+									player.sendMessage(ChatColor.GOLD
+											+ messageInsufficient);
+									player.getInventory().remove(
+											player.getItemInHand());
+
+								}
+							} else {
+								player.sendMessage(ChatColor.GOLD
+										+ messageUnable);
+							}
+						}
 					}
 				}
 			}
 		}
 	}
-}
 }
